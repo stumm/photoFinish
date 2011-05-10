e6019d410c1defaf894989f9e41a3151ad791792
Christopher Stumm <christopher@stumm.ca>
2011-05-09 22:44:19 -0700

    .

--- photoFinish.py
+++ photoFinish.py
@@ -40,14 +40,28 @@ def combineImages(imageDir):
     # need to figure out number of images, so we can create the pano image
     # to the right size
     numImages = len(glob.glob(imageDir + "*.jpg"))
-    print numImages
 
     # create pano image
+    # this is stupid -> should just grab an image instead of creating iterator
+    for imagePath in glob.iglob(imageDir + "*.jpg"):
+        im = Image.open(imagePath)
+        x_size, y_size = im.size
+        break # all images should be the same size
 
-    for image in glob.iglob(imageDir + "*.jpg"):
-        print image
+    panoImg = Image.new(im.mode, (numImages, y_size))
+
+    x_offset = 0
+    box = (0, 0, 1, y_size)
+    for imagePath in glob.iglob(imageDir + "*.jpg"):
         # grab vertical set of pixels
-        # add to existing image
+        im = Image.open(imagePath)
+        slit = im.crop(box)
+
+        # paste in pano image, incrementing the x_offset as we go
+        panoImg.paste(slit, (x_offset, 0, x_offset + 1, y_size))
+        x_offset += 1
+
+    panoImg.save("test.jpg", "JPEG")
 
 # input: movieFileName fileName
 if __name__ == "__main__":
